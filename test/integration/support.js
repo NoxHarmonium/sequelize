a8e345ffc296c6f6f28d52e0121e6a2c519e6601
Sean Dawson <contact@seandawson.info>
2019-10-16 18:27:20 +1100

    Fix to support table sampling

--- test/integration/support.js
+++ test/integration/support.js
@@ -1,6 +1,17 @@
 'use strict';
 
-var Support = require('../support');
+var Support = require('../support')
+  , dialect = Support.getTestDialect();
+
+before(function() {
+  if (dialect !== 'postgres' && dialect !== 'postgres-native') {
+    return;
+  }
+  return Support.sequelize.Promise.all([
+    Support.sequelize.query('CREATE EXTENSION IF NOT EXISTS hstore', {raw: true}),
+    Support.sequelize.query('CREATE EXTENSION IF NOT EXISTS btree_gist', {raw: true})
+  ]);
+});
 
 beforeEach(function() {
   this.sequelize.test.trackRunningQueries();
